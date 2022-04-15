69daa0750b819df03eb29af40f4072d160d936e8
Johan Laanstra <jlaans@microsoft.com>
2022-04-14 18:31:20 -0700

    Enable single instancing.

---
+++ WinAppSDKApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinAppSDKApp
+{
+#if DISABLE_XAML_GENERATED_MAIN
+    public static class Program
+    {
+        private static App app;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            global::WinRT.ComWrappersSupport.InitializeComWrappers();
+
+            var mainInstance = AppInstance.FindOrRegisterForKey("main");
+            if (mainInstance.IsCurrent)
+            {
+                global::Microsoft.UI.Xaml.Application.Start((p) =>
+                {
+                    var syncContext = new WinAppSDKApp.DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                    SynchronizationContext.SetSynchronizationContext(syncContext);
+
+                    app = new App();
+                });
+
+                mainInstance.UnregisterKey();
+            }
+            else
+            {
+                RedirectActivationToAsync(mainInstance, AppInstance.GetCurrent().GetActivatedEventArgs()).Wait();
+            }
+        }
+
+        private static async Task RedirectActivationToAsync(AppInstance mainInstance, AppActivationArguments activationArgs)
+        {
+            await TaskEx.YieldToBackground();
+
+            await mainInstance.RedirectActivationToAsync(activationArgs);
+        }
+    }
+#endif
+}
